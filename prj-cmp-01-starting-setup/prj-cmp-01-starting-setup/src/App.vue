295528b790d200c60b58ad25db69fae27c5ac289
Shynu Mary Varghese <Svarghese2938@conestogac.on.ca>
2021-07-16 05:30:21 +0530

    Section 10 chapter 125

---
+++ prj-cmp-01-starting-setup/prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul>
+    <!-- bind all props and provide values to all props in learning resources -->
+    <learning-resources
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.describtion"
+      :link="res.link"
+    >
+    </learning-resources>
+  </ul>
+</template>
+<script>
+import LearningResources from './components/learning-resources/LearningResource.vue';
+export default {
+  components: {
+    LearningResources
+  },
+
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          describtion: 'The Official Vue js documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'Google',
+          title: 'Google',
+          describtion: 'Learn to google',
+          link: 'https://google.org'
+        }
+      ]
+    };
+  }
+};
+</script>
---
+++ prj-cmp-01-starting-setup/prj-cmp-01-starting-setup/src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,25 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <!-- Title -->
+        <h3>{{ title }}</h3>
+        <button>
+          Delete
+        </button>
+      </header>
+    </div>
+    <!-- Description -->
+    <p>{{ description }}</p>
+    <nav>
+      <!-- Link -->
+      <a v-bind:href="link">View Resource</a>
+    </nav>
+  </li>
+</template>
+<script>
+export default {
+  // with props : gets external data by making components reusable and configurable
+  props: ['title', 'description', 'link']
+};
+</script>
